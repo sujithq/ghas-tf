49b222ff7fce3dee5b9350caf24c087ef1ef54bd
Sujith Quintelier <sujith.quintelier@gmail.com>
2024-04-20 11:09:59 +0200

    Update

--- main.tf
+++ main.tf
@@ -1,12 +1,14 @@
 resource "azurerm_resource_group" "this" {
-    location = "westeurope"
-    name = "rg-ghas-tf"
+  location = "westeurope"
+  name     = "rg-ghas-tf"
+  tags     = local.tags
 }
 
 resource "azurerm_network_security_group" "example" {
   name                = "nsg-ghas-tf"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
+  tags                = azurerm_resource_group.this.tags
 
   security_rule {
     name                       = "allow-all"
@@ -15,7 +17,7 @@ resource "azurerm_network_security_group" "example" {
     access                     = "Allow"
     protocol                   = "*"
     source_port_range          = "*"
-    destination_port_range     = "1433"  # Typically used for SQL Server
+    destination_port_range     = "1433" # Typically used for SQL Server
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
