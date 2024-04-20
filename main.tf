53d6cfcc767a2dc0322601960c82900d62e76539
Sujith Quintelier <sujith.quintelier@gmail.com>
2024-04-20 11:00:15 +0200

    Initial version

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "this" {
+    location = "westeurope"
+    name = "rg-ghas-tf"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "nsg-ghas-tf"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  security_rule {
+    name                       = "allow-all"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "1433"  # Typically used for SQL Server
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
